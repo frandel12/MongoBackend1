faa2fa30c9a3b41a4a37cd3c0782858cf3e6b15d
Javi <Javi@FRAN>
2022-12-09 23:59:25 -0600

    Agregar archivos de proyecto.

---
+++ MongoBackend1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoBackend1.Models;
+using System;
+using System.Diagnostics;
+
+namespace MongoBackend1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DatabaseHelper.Database db = new DatabaseHelper.Database();
+            ViewBag.UserList = db.getUsers();
+
+            return View();
+        }
+
+        public IActionResult insertUser(string txtname, string txtaddress, int txtphone, string txtemail)
+        {
+            DatabaseHelper.Database db = new DatabaseHelper.Database();
+            db.insertUser(new User()
+            {
+                name = txtname,
+                email = txtemail,
+                phone = txtphone,
+                address = txtaddress,
+                dateIn = DateTime.Now
+            });
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult updateUser(string txtid, string txtname, string txtaddress, int txtphone, string txtemail)
+        {
+            DatabaseHelper.Database db = new DatabaseHelper.Database();
+
+            db.updateUser(txtid, txtname, txtaddress, txtphone, txtemail);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult deleteUser(string txtid)
+        {
+            DatabaseHelper.Database db = new DatabaseHelper.Database();
+            User user = new User()
+            {
+                _id = ObjectId.Parse(txtid)
+            };
+            db.deleteUser(user);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MongoBackend1/DatabaseHelper/Database.cs
@@ -0,0 +1,87 @@
+﻿using MongoBackend1.Models;
+using System.Text.Json;
+using System;
+
+namespace MongoBackend1.DatabaseHelper
+{
+    public class Database
+    {
+        public List<User> getUsers()
+        {
+            MongoClient mongoClient = new MongoClient("mongodb+srv://admin:$1234@cluster0.fg4a0fu.mongodb.net/test");
+
+            IMongoDatabase db = mongoClient.GetDatabase("MongoBackend");
+            var users = db.GetCollection<BsonDocument>("Users");
+            List<BsonDocument> userArray = users.Find(new BsonDocument()).ToList();
+
+            List<User> userList = new List<User>();
+
+            foreach (BsonDocument bsonUser in userArray)
+            {
+                User user = BsonSerializer.Deserialize<User>(bsonUser);
+                userList.Add(user);
+            }
+
+            return userList;
+        }
+
+        public void insertUser(User user)
+        {
+            MongoClient mongoClient = new MongoClient("mongodb+srv://admin:$1234@cluster0.fg4a0fu.mongodb.net/test");
+            IMongoDatabase db = mongoClient.GetDatabase("MongoBackend");
+
+            var users = db.GetCollection<BsonDocument>("Users");
+
+            var doc = new BsonDocument
+            {
+                { "name", user.name },
+                { "email", user.email },
+                { "phone", user.phone },
+                { "address", user.address},
+                { "dateIn", user.dateIn }
+            };
+
+            users.InsertOne(doc);
+        }
+
+
+        public void deleteUser(User user)
+        {
+            MongoClient mongoClient = new MongoClient("mongodb+srv://admin:$1234@cluster0.fg4a0fu.mongodb.net/test");
+            IMongoDatabase db = mongoClient.GetDatabase("MongoBackend");
+
+            var users = db.GetCollection<BsonDocument>("Users");
+            var deleteFilter = Builders<BsonDocument>.Filter.Eq("_id", user._id);
+
+            users.DeleteOne(deleteFilter);
+        }
+
+        public void updateUser(string txtid, string txtname, string txtaddress, int txtphone, string txtemail)
+        {
+            MongoClient mongoClient = new MongoClient("mongodb+srv://admin:$1234@cluster0.fg4a0fu.mongodb.net/test");
+
+            IMongoDatabase db = mongoClient.GetDatabase("MongoBackend");
+
+            var users = db.GetCollection<BsonDocument>("Users");
+
+            var doc = new BsonDocument
+            {
+                { "_id", ObjectId.Parse(txtid) }
+            };
+
+
+            var doc1 = new BsonDocument
+            {
+                { "_id", ObjectId.Parse(txtid) },
+                {"name", txtname },
+                { "email", txtemail} ,
+                { "phone", txtphone},
+                 {"address", txtaddress },
+
+            };
+
+            users.ReplaceOne(doc, doc1);
+
+        }
+    }
+}
